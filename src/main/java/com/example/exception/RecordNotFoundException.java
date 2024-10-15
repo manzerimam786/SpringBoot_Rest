b0f18be5ba294a6423300d55682ee205b5f9a96f
Md Manzer Imam <manzerimam786@gmail.com>
2024-10-15 17:49:02 +0530

    adding changes for Data JPA + jacoco + jasypt

--- src/main/java/com/example/exception/RecordNotFoundException.java
+++ src/main/java/com/example/exception/RecordNotFoundException.java
@@ -1,11 +1,10 @@
-package com.example;
+package com.example.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
- 
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class RecordNotFoundException extends RuntimeException 
-{
+public class RecordNotFoundException extends Exception {
     public RecordNotFoundException(String exception) {
         super(exception);
     }
